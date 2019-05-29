fbb666bc9dc0775aef3160504516a8c50fa5bae0
Bishal Bhandari <bishalbhaandari62@gmail.com>
2019-05-29 12:51:00 +0545

    File of html and JS created

---
+++ script.js
@@ -0,0 +1,14 @@
+   var fName = prompt("Please enter your first Name");
+   var lName = prompt("Please enter your last Name");
+   var Age = prompt("Please enter your age");
+   var Gender = prompt("Please enter your gender");
+
+   var data = {
+       fName: fName,
+       lName:lName,
+       age:Age,
+       gender:Gender,
+       fullname:function(){return(this.fName + this.lName)}
+   }
+
+alert("Name"+data.fullname()+"\n"+"Age"+data.age+"\n"+"Gender"+data.gender);
